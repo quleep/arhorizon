0dd6a27c86b1e14293025168df27f40d0c2e412c
Omanshuamanarnxt <omanshuaman@email.com>
2023-12-14 12:42:33 +0530

    react router pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,15 @@
-import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import axios from "axios";
-
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Campaign from "./pages/Campaign";
 function App() {
-  const [responseData, setResponseData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [count, setCount] = useState(0);
-  const urlParams = new URLSearchParams(window.location.search);
-  const paramValue = urlParams.get("");
-  console.log(paramValue); // Output: "dcd"
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Make a GET request using Axios
-        const response = await axios.get(
-          `https://3ef9gn5kk2.execute-api.ap-south-1.amazonaws.com/arnxt_prod/ar-horizon/uploadtargetimage?id=${paramValue}`
-        );
-        console.log(response.data);
-        // Set the response data in state
-        setResponseData(response.data);
-        setLoading(false);
-      } catch (error) {
-        // Handle errors
-        console.error("Error fetching data:", error);
-        setError("Error fetching data");
-        setLoading(false);
-      }
-    };
-
-    // Call the fetchData function
-    fetchData();
-  }, [paramValue]);
   return (
-    <>
-      {responseData && (
-        <a-scene
-          vr-mode-ui="enabled: false;"
-          loading-screen="enabled: false;"
-          renderer="logarithmicDepthBuffer: true;"
-          arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false;"
-          id="scene"
-          embedded
-          gesture-detector>
-          <a-assets>
-            <a-entity
-              id="animated-asset"
-              src={responseData.TargetGlbFile}></a-entity>
-          </a-assets>
-
-          <a-marker
-            id="animated-marker"
-            type="pattern"
-            preset="custom"
-            url={responseData.TargetImagePattFile}
-            raycaster="objects: .clickable"
-            emitevents="true"
-            cursor="fuse: false; rayOrigin: mouse;">
-            <a-entity
-              id="bowser-model"
-              scale="2.078150572818595 2.078150572818595 2.078150572818595"
-              animation-mixer1="loop: repeat"
-              gltf-model="#animated-asset"
-              class="clickable"
-              gesture-handler></a-entity>
-          </a-marker>
-
-          <a-entity camera></a-entity>
-        </a-scene>
-      )}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/campaign" element={<Campaign />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
