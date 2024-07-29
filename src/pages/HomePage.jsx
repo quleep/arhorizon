3f68b4960774cb1b34349d833d0645c86ede6fad
Omanshuamanarnxt <omanshuaman@email.com>
2024-07-29 12:41:15 +0530

    optimize code

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,35 @@
-import React from "react";
 import { Hero, Navbar, Works } from "../components";
 import HowItWorks from "../../public/assets/videos/frame1.gif";
 import { styles } from "../styles";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 function HomePage() {
+  const [location, setLocation] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchLocation = async () => {
+      try {
+        const response = await axios.get("https://geolocation.microlink.io/");
+        setLocation(response.data);
+        console.log(response.data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchLocation();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
   return (
     <div className="relative z-0 bg-lightdark">
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Test() {
-  return <div>Test</div>;
+  return (
+    <div className="h-screen w-screen">
+      <iframe
+        src="https://192.168.0.107:5173/grid_AR/1.html?id=1720785564550"
+        style={{ height: "100%", width: "100%" }}
+        title="Iframe Example"></iframe>
+    </div>
+  );
 }
 
 export default Test;
